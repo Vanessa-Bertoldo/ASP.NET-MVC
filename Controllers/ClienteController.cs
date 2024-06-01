8f00f0411c1dc86dd6dfb940696230c621fdc427
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-06-01 18:43:07 -0300

    insert form for create client

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using web.students.Models;
 
 namespace web.students.Controllers
@@ -6,9 +7,11 @@ namespace web.students.Controllers
     public class ClienteController : Controller
     {
         public IList<ClienteModel> clientes { get; set; }
+        public IList<RepresentanteModel> representantes { get; set; }
         public ClienteController() 
         {
             clientes = GerarClientesMocados();
+            representantes = GerarRepresentantesMocados();
         }
 
         public static List<ClienteModel> GerarClientesMocados()
@@ -47,17 +50,44 @@ namespace web.students.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            Console.WriteLine("EXECUTOU A ACTION CADASTRAR()");
+            Console.WriteLine("Executou a Action Cadastrar()");
+            //Cria a variável para armazenar o SelectList
+            var selectListRepresentantes =
+                new SelectList(representantes,
+                                nameof(RepresentanteModel.RepresentanteId),
+                                nameof(RepresentanteModel.NomeRepresentante));
+            //Adiciona o SelectList a ViewBag para se enviado para a View
+            //A propriedade Representantes é criada de forma dinâmica na ViewBag
+            ViewBag.Representantes = selectListRepresentantes;
+            // Retorna para a View Create um 
+            // objeto modelo com as propriedades em branco 
             return View(new ClienteModel());
         }
-
+        // Anotação de uso do Verb HTTP Post
         [HttpPost]
-        public IActionResult Create(ClienteModel cliente)
+        public IActionResult Create(ClienteModel clienteModel)
         {
-            Console.WriteLine("GRAVADNO O CLKIENTE");
-            TempData["mensagemSucesso"] = $"O cliente {cliente.Nome} foi cadastrado com sucesso";
+            // Simila que os dados foram gravados.
+            Console.WriteLine("Gravando o cliente");
+            //Criando a mensagem de sucesso que será exibida para o Cliente
+            TempData["mensagemSucesso"] = $"O cliente {clienteModel.Nome} foi cadastrado com suceso";
+            // Substituímos o return View()
+            // pelo método de redirecionamento
             return RedirectToAction(nameof(Index));
+            // O trecho nameof(Index) poderia ser usado da forma abaixo
+            // return RedirectToAction("Index");
+        }
 
+        public static List<RepresentanteModel> GerarRepresentantesMocados()
+        {
+            var representantes = new List<RepresentanteModel>
+            {
+                new RepresentanteModel { RepresentanteId = 1, NomeRepresentante = "Representante 1", Cpf = "111.111.111-11" },
+                new RepresentanteModel { RepresentanteId = 2, NomeRepresentante = "Representante 2", Cpf = "222.222.222-22" },
+                new RepresentanteModel { RepresentanteId = 3, NomeRepresentante = "Representante 3", Cpf = "333.333.333-33" },
+                new RepresentanteModel { RepresentanteId = 4, NomeRepresentante = "Representante 4", Cpf = "444.444.444-44" }
+            };
+            return representantes;
         }
     }
 }
--- obj/Debug/net8.0/web.students.AssemblyInfo.cs
+++ obj/Debug/net8.0/web.students.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web.students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883089b1cde97ebd531a6d123351392c50f14bff")]
 [assembly: System.Reflection.AssemblyProductAttribute("web.students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web.students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
