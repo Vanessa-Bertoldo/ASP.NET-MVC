2090e0d28d0a887500e0865805e91de5c0a3bad9
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-06-20 21:21:54 -0300

    insert migration clientes

--- Data/Contexts/DatabaseContext.cs
+++ Data/Contexts/DatabaseContext.cs
@@ -5,9 +5,9 @@ namespace web.students.Data.Contexts
 {
     public class DatabaseContext : DbContext
     {
-        // TODO: propriedade para manipular a entidade de representante
+        // TODO: propriedade para manipular as entidades
         public DbSet<RepresentanteModel> Representantes { get; set; }
-
+        public DbSet<ClienteModel> Cliente { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<RepresentanteModel>(entity =>
@@ -20,6 +20,24 @@ namespace web.students.Data.Contexts
                 entity.Property(e => e.NomeRepresentante).IsRequired();
                 //Adicionando indice unico para CPF
                 entity.HasIndex(e => e.Cpf).IsUnique();
+            });
+
+            modelBuilder.Entity<ClienteModel>(entity =>
+            {
+            entity.ToTable("Clientes");
+            entity.HasKey(e => e.ClienteId);
+            entity.Property(e => e.Nome).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.DataNascimento).HasColumnType("date");
+            entity.Property(e => e.Observacao).HasMaxLength(500);
+            //Define a relação de um pra um com RepresentanteModel
+            entity.HasOne(e => e.Representante)
+                //Indica que um representante pode ter muitos clientes
+                .WithMany()
+                //Define a chave estrangeira
+                .HasForeignKey(e => e.RepresentanteId)
+                // Torna a chave estrangeira obrigatória
+                .IsRequired();
             });
         }
         public DatabaseContext(DbContextOptions options) : base(options) { }
