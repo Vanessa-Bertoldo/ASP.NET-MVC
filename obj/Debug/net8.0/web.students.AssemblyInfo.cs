a02a4fe0b10bc8a3b908ec4e5b33b2ad8f84a024
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-06-22 14:39:11 -0300

    insert new Tests project

--- obj/Debug/net8.0/web.students.AssemblyInfo.cs
+++ obj/Debug/net8.0/web.students.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web.students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef9ffb9240a56e7da2d478a7085eada4225ea0cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02bf4e4f00b011deb2ff1aabad57375f48640566")]
 [assembly: System.Reflection.AssemblyProductAttribute("web.students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web.students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
